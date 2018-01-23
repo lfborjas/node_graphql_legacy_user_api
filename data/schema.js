0cc9fc01a9c562c6c180612f90975e482f50cd1e
Luis Borjas <luis@birchbox.com>
2018-01-23 15:45:24 -0500

    initial commit from cloned template

--- data/schema.js
+++ data/schema.js
@@ -1,14 +1,27 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
-import mocks from './mocks';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+
 
 const typeDefs = `
-type Query {
-  testString: String
+type User {
+  id: Int,
+  first_name: String,
+  last_name: String,
+  email: String,
+  point_balance: Int
+  subscriptions: [Subscription]
+}
+
+scalar Date
+
+type Subscription {
+  name: String,
+  is_active: Boolean,
+  next_ship_date: Date
 }
 `;
 
 const schema = makeExecutableSchema({ typeDefs });
-
-addMockFunctionsToSchema({ schema, mocks });
 
 export default schema;
