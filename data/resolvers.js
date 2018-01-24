5b7324e597bf66ccc965e9ca9249fee9e9ba2f9d
Luis Borjas <luis@birchbox.com>
2018-01-24 17:51:51 -0500

    simplify dates

--- data/resolvers.js
+++ data/resolvers.js
@@ -2,24 +2,10 @@ import { Kind } from 'graphql/language';
 import { GraphQLScalarType } from 'graphql';
 var models = require('../models');
 
+// we could have _actual_ dates vs strings:
 // from https://www.apollographql.com/docs/graphql-tools/scalars.html#Date-as-a-scalar
+// + https://github.com/tjmehta/graphql-date/blob/master/index.js
 const resolvers = {
-  Date: new GraphQLScalarType({
-    name: "Date",
-    description: 'Date scalar',
-    parseValue(value){
-      return new Date(value);
-    },
-    serialize(value){
-      return value.getTime()
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return parseInt(ast.value, 10); // ast value is always in string format
-      }
-      return null;
-    }
-  }),
   Query: {
     user(root, args){
       return models.user.findOne({where: args});
